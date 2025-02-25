fa9b09925283fee915b1d6770d6581a5349e617c
Harshthakur <yashthakur0729@example.com>
2025-02-25 12:53:31 +0530

    Like and unlike changes

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,7 +1,7 @@
 import postModel from "../models/post.model.js";
 import { generateCaptionFromImageBuffer } from "../services/ai.service.js";
 import { uploadFile } from "../services/cloudStorage.service.js";
-
+import likeModel from "../models/likes.model.js";
 export const createPost = async (req, res) => {
     try {
         const imageBuffer = req.file?.buffer;
@@ -21,8 +21,60 @@ export const createPost = async (req, res) => {
         res.status(201).json({ post: newPost, message: "Post created successfully" })
     } catch (error) {
         console.log(error);
-        
+
         res.status(500).json({ message: "Internal server error" })
     }
 
+}
+
+
+export const likePost = async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        console.log(postId);
+
+        if (!postModel.isValidPostId(postId)) {
+            return res.status(400).json({ message: "Invalid post id" })
+        }
+        const post = await postModel.findById(postId);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" })
+        }
+        const isAlreadyLiked = await likeModel.findOne({ post: postId, user: req.user._id })
+        if (isAlreadyLiked) {
+            return res.status(400).json({ message: "You have already liked this post" })
+        }
+        await likeModel.create({ post: postId, user: req.user._id })
+        await post.incrementLikeCount();
+        res.status(200).json({ message: "Post liked successfully" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+
+export const removeLikePost = async (req, res, next) => {
+    try {
+        const postId = req.params.postId;
+        if (!postModel.isValidPostId(postId)) {
+            return res.status(400).json({ message: "Invalid post id" })
+        }
+        const post = await postModel.findById(postId);
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" })
+        }
+        const userLikedPost = await likeModel.findOne({ post: postId, user: req.user._id })
+        if (!userLikedPost) {
+            return res.status(200).json({ message: "You have not liked this post" })
+        }
+        await likeModel.findOneAndDelete({ post: postId, user: req.user._id });
+        await post.decrementLikeCount();
+        await post.save();
+
+        res.status(200).json({ message: "Post unliked successfully" })
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createPost } from "../controller/post.controller.js";
+import { createPost, likePost, removeLikePost } from "../controller/post.controller.js";
 import { handleFileUpload } from "../middleware/post.middleware.js";
 import { authUser } from "../middleware/user.middleware.js";
 
 const router = Router();
 
 router.post("/create", handleFileUpload, authUser, createPost);
+router.patch("/like/:postId", authUser, likePost);
+router.patch("/unlike/:postId", authUser, removeLikePost);
 
 export default router;
